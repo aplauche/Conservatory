1f1ed055babd17febdb06c59a448ba67044766e8
Anton Plauche <antonplauche92@gmail.com>
2023-06-25 18:01:22 -0500

    unbranded branch for demo purposes

--- src/components/MainOverlay.js
+++ src/components/MainOverlay.js
@@ -12,13 +12,11 @@ export default function MainOverlay(){
             <img src="/images/gc-logo.svg" alt="" />
           </div>
           <div className="header-intro pt-12">
-            <span className="bg-light-green rounded-full px-4 py-2 text-white inline-block mb-12">Garfield Park Conservatory</span>
+            <span className="bg-light-green rounded-full px-4 py-2 text-white inline-block mb-12">The Virtual Conservatory</span>
             <p className="text-3xl mb-0 font-bold text-black">Explore the</p>
             <h1 className="text-6xl font-bold mb-12 text-black">Conservatory.</h1>
             <p className="mb-6">
-            The Garfield Park Conservatory in Chicago, Illinois is one of the largest and most stunning conservatories in the nation. 
-            </p>
-            <p>Often referred to as “landscape art under glass,” the Garfield Park Conservatory occupies two acres of public greenhouse space and 10 acres of outdoor gardens. </p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra lorem a lacus porta suscipit. Morbi dolor dolor, lobortis vel urna vel, semper mollis est. Nam dictum vel quam vel convallis. Pellentesque eu metus tincidunt, condimentum velit at, efficitur mauris. Morbi lectus nulla, efficitur a mattis at, vulputate ut est.            </p>
           </div>
         </div>
         {/* <a href="https://garfieldconservatory.org/" target="_blank" className="btn-primary mt-12">Visit Main Site</a> */}
--- src/stores/roomsData.js
+++ src/stores/roomsData.js
@@ -4,7 +4,7 @@ export default [
     name: "Palm House",
     targetScale: {x: 4, y: 4.5, z: 10.5},
     targetPos: {x: 3.45, y: 0, z:0.2},
-    description: "At 65 ft high and 90 ft wide, this is the largest room in the Conservatory. It is designed as an idealized tropical landscape, featuring more than 70 graceful palms, as well as other plants from warm habitats all around the world.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra lorem a lacus porta suscipit. Morbi dolor dolor, lobortis vel urna vel, semper mollis est. Nam dictum vel quam vel convallis.",
     photo: "/images/palm-house-2.jpg",
     cameraShift: {x:20, y:20, z: 20},
     focusRotation: {x:0, y: -Math.PI / 1.5, z: 0},
@@ -35,7 +35,7 @@ export default [
     name: "Fern Room",
     targetScale: {x: 5.5, y: 4.5, z: 5},
     targetPos: {x: -1.25, y: 0, z:0.2},
-    description: "Jens Jensen, who designed the Conservatory in 1906, wanted to give visitors a glimpse of what Illinois might have looked like millions of years ago. Lush ferns, rocky outcroppings and an indoor lagoon evoke the swampy landscape of prehistoric Chicago.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra lorem a lacus porta suscipit. Morbi dolor dolor, lobortis vel urna vel, semper mollis est. Nam dictum vel quam vel convallis.",
     photo: "/images/fern-room.jpg",
     cameraShift: {x:34, y:14, z: 12},
     focusRotation: {x:0.8, y: -1, z: 0},
@@ -49,6 +49,14 @@ export default [
       {
         title: "Humidity",
         stat: "60%"
+      },
+      {
+        title: "Species",
+        stat: "500+"
+      },
+      {
+        title: "Square Feet",
+        stat: "10K+"
       }
     ]
   },
@@ -57,7 +65,7 @@ export default [
     name: "Show House",
     targetScale: {x: 5.5, y: 4.5, z: 2.6},
     targetPos: {x: -1, y: 0, z: -3.3},
-    description: "This room is the site of spectacular flower shows each year. Much of the plant material displayed in the shows is grown at the Conservatory. The original 1908 layout of the room was restored following the 2011 hailstorm, and includes the large central ellipse that was designed as the feature display for flower shows.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra lorem a lacus porta suscipit. Morbi dolor dolor, lobortis vel urna vel, semper mollis est. Nam dictum vel quam vel convallis.",
     photo: "/images/showroom.png",
     pano: "/images/panos/show-room.jpg",
     cameraShift: {x:34, y:14, z: 12},
@@ -79,7 +87,7 @@ export default [
     name: "Sugar from the Sun",
     targetScale: {x: 5.5, y: 4.5, z: 2.6},
     targetPos: {x: -1, y: 0, z:3.45},
-    description: "Four themed botanical environments – water, air, sunlight, and sugar – help visitors discover how right now, inside every leaf, plants are capturing sunlight and using it to change small parts of air and water into sugar – the energy that sustains life on Earth.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra lorem a lacus porta suscipit. Morbi dolor dolor, lobortis vel urna vel, semper mollis est. Nam dictum vel quam vel convallis.",
     photo: "/images/sugar-from-the-sun.png",
     pano: "/images/panos/sugar-from-the-sun.jpg",
     cameraShift: {x:-30, y:25, z: -10},
@@ -101,7 +109,7 @@ export default [
     name: "Desert House",
     targetScale: {x: 2.5, y: 4.5, z: 5},
     targetPos: {x: -5.5, y: 0, z:0},
-    description: "The Desert House holds one of the region’s most varied collections of cacti and succulents. These plants owe their popularity to their spectacular and unique forms, the promise of brilliant, short-lived flowers and their ability to withstand harsh, dry conditions. The size of the plants in this room varies from the tiny living stone plant to the large century plants.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra lorem a lacus porta suscipit. Morbi dolor dolor, lobortis vel urna vel, semper mollis est. Nam dictum vel quam vel convallis.",
     photo: "/images/desert-room.png",
     pano: "/images/panos/desert-room.jpg",
     cameraShift: {x:0, y:25, z: -40},
