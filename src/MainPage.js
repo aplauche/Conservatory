8f8c150112ed6618bb46124c5fcecd27e8c5cba7
Anton Plauche <antonplauche92@gmail.com>
2023-07-06 21:30:30 -0500

    conditional instructions

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber"
 import MainScene from "./MainScene"
 import Drawer from './components/Drawer.js'
-import { Suspense, useEffect, useLayoutEffect } from "react"
+import { Suspense, useEffect, useLayoutEffect, useState } from "react"
 import { Loader, useProgress } from "@react-three/drei"
 import MainOverlay from "./components/MainOverlay"
 import useRoom from "./stores/useRoom"
@@ -18,8 +18,27 @@ export default function MainPage(){
 
   const exitPanoScene = useRoom((state) => state.exitPanoScene)
 
+  const [isMobile, setIsMobile] = useState(false)
+
+  const handleResize =() => {
+    if (window.innerWidth < 769){
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+
   useLayoutEffect(() => {
     exitPanoScene()
+  }, [])
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
   }, [])
 
 
@@ -38,13 +57,18 @@ export default function MainPage(){
     
           </Canvas>
 
-          <Instructions keyVal="main" isVisible={!currentlySelected} position="bottom">
-            Click and drag to rotate
-          </Instructions>
+          {!isMobile && (
+            <>
+              <Instructions keyVal="main" isVisible={!currentlySelected} position="bottom">
+                Click and drag to rotate
+              </Instructions>
 
-          <Instructions keyVal="detail" isVisible={currentlySelected != null} position="top">
-            Click to return to main view
-          </Instructions>
+              <Instructions keyVal="detail" isVisible={currentlySelected != null} position="top">
+                Click to return to main view
+              </Instructions>
+            </>
+          )}
+
 
         </div>
         <Drawer /> 
