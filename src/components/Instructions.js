cfca51ae4162da86ee226090aae7f42033ef4549
Anton Plauche <antonplauche92@gmail.com>
2023-07-10 08:26:11 -0500

    center text

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -14,7 +14,7 @@ function Instructions({keyVal, isVisible=true, position="top", children}) {
         <motion.div
           key={keyVal}
           style={styling}
-          className={`pointer-events-none absolute bg-white rounded-full py-2 px-5 lg:py-4 lg:px-8 left-1/2 -translate-x-1/2`}
+          className={`text-center pointer-events-none absolute bg-white rounded-full py-2 px-5 lg:py-4 lg:px-8 left-1/2 -translate-x-1/2`}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
